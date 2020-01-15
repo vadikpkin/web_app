f30c3915406e71dfe92e6ee6b417253e666e1c80
vadik <pkinchek@gmail.com>
2020-01-15 12:06:35 +0300

    responce fix

---
+++ init/init.sql
@@ -0,0 +1,17 @@
+create table persons(
+id serial not null primary key,
+name varchar(255) not null,
+surname varchar(255) not null,
+email varchar(255) not null unique,
+type varchar(30) not null
+);
+
+create table ads(
+person_id integer references persons (id),
+id varchar(255) not null primary key,
+title varchar(255) not null,
+body text not null,
+category varchar(30) not null,
+phone varchar(30) not null,
+dateOfCreation date not null
+);
